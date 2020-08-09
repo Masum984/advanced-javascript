e784986ebb1ed5d8828f563b4901512113eb2966
Masum984 <66863097+Masum984@users.noreply.github.com>
2020-08-09 11:56:51 +0600

    null-vs-undefined class

---
+++ true-false.js
@@ -0,0 +1,19 @@
+const name = "Solaiman";
+let name = null;
+
+if( name.length>0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false");
+}
+//Falsy;
+//false
+//0;
+//""
+// undefined
+//null
+//NaN
+//Truthy
+//lat 
+// '0', ''[]
